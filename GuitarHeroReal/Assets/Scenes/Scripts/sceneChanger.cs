d87b8f54b8de0a4d67dfa9489c15e6c20359b12f
Seth <sethhukill2018@gmail.com>
2020-11-04 19:12:48 -0500

    Adding game files

---
+++ GuitarHeroReal/Assets/Scenes/Scripts/sceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneChanger : MonoBehaviour
+{
+    public void toWii()
+    {
+        SceneManager.LoadScene("Wii Song");
+    }
+}
---
+++ GuitarHeroReal/Assets/Scenes/Scripts/toSticker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class toSticker : MonoBehaviour
+{
+    public void toStickerSong()
+    {
+        SceneManager.LoadScene("Sticker Song");
+    }
+}
+
