3e8bc501e6b166460480e83f8b31e14417d37581
Ethan37 <brown.ethan9@gmail.com>
2020-11-13 18:42:15 -0500

    Update project

---
+++ CIS 350 Guitar Hero/Assets/Scripts/Note.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+  
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        rb.velocity = new Vector2(0, -speed);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
