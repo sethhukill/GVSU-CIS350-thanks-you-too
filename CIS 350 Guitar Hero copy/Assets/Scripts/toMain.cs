32d0d9ac6d901e57594c2daaa7e81ab41683164c
Seth <sethhukill2018@gmail.com>
2020-11-17 18:51:38 -0500

    Updated game with pause menu

---
+++ CIS 350 Guitar Hero copy/Assets/Scripts/toMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class toMain : MonoBehaviour
+{
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
